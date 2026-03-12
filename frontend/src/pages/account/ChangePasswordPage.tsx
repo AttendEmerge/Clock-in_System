05240180006d89dfc619d521510ba1a4a2b00117
Abel A. Akalu <abel.abebe.akalu@gmail.com>
2026-03-12 15:10:17 +0200

    password resets

--- frontend/src/pages/account/ChangePasswordPage.tsx
+++ frontend/src/pages/account/ChangePasswordPage.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useState } from 'react';
+import type { FormEvent } from 'react';
+import { useState } from 'react';
 import Layout from '../../components/Layout';
 import { changePassword } from '../../services/api';
 
